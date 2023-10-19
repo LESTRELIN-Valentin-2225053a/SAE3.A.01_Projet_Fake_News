e8656417dac8a8217c476945c8a8fa5e4cc7d32d
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-10-19 11:23:18 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,38 +8,49 @@ import javafx.scene.text.*;
 import javafx.scene.image.*;
 import javafx.geometry.Pos;
 import javafx.animation.*;
-import javafx.util.Duration;
 import java.time.*;
+import javafx.util.Duration;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main extends Application {
   int screenW = 900;
   int screenH = 600;
-  long mainMsTime = 0;
-  float opc = 0;
-
-  public float objectOpacity(float msTimeBegin, float msTimeEnd, float opacityBegin, float opacityEnd, float defaultOpacity) {
-    if (msTimeBegin <= mainMsTime && mainMsTime <= msTimeEnd) {
-      return opacityBegin + (opacityEnd - opacityBegin) * ((mainMsTime - msTimeBegin) / (msTimeEnd - msTimeBegin));
-    }  
-    else {
-      return defaultOpacity;
-    }
-  }
+  Map<AnImage, List<Integer>> imagesAppearances = new HashMap<>();
+  Map<AText, List<Integer>> textsAppearances = new HashMap<>();
+  List<Integer> imagesMs = new ArrayList<>();
+  List<Integer> textsMs = new ArrayList<>();
+  int mainMsTime = 0;
   
-  public void start(Stage primaryStage) {
+  public void start(Stage primaryStage) {    
     Rectangle mainBg = new Rectangle(0,0,900,600);
     mainBg.setFill(Color.rgb(255,255,255));
-    AnImage img = new AnImage("IUTLogo.png", screenW * 0.5 - screenW * 0.25, screenH * 0.1, screenW * 0.5, screenH * 0.2);
+    
+    AnImage iutLogo = new AnImage("IUTLogo.png", screenW * 0.5 - screenW * 0.25, screenH * 0.1, screenW * 0.5, screenH * 0.2);
+    imagesMs = List.of(0, 5000);
+    imagesAppearances.put(iutLogo, imagesMs);
+    
     AText creators = new AText("M. Thiel, R. Lerouge, V. Lestrelin, M. Ditlecadet,\nA. Essalhi et M. Brahmi présentent", 0, 0, 0, "Arial", screenW * 0.125, screenH * 0.5, screenW * 0.75, screenH / 6);
-    /*
-    StackPane sp = new StackPane();
-    sp.setMinWidth(screenW);
-    sp.setMinHeight(screenH);
-    sp.getChildren().add(title);
-    sp.setAlignment(title,Pos.CENTER);
-    */
+    textsMs = List.of(0, 5000);
+    textsAppearances.put(creators, textsMs);
+
+    AText forDaily = new AText("Pour le site d'information marseillais", 0 , 0, 0, "Arial", screenW * 0.125, screenH * 0.1, screenW * 0.75, screenH / 6);
+    textsMs = List.of(5000, 10000);
+    textsAppearances.put(forDaily, textsMs);
+    
+    AnImage marsactuLogo = new AnImage("MarsactuLogo.png", screenW * 0.5 - screenW * 0.25, screenH * 0.5, screenW * 0.5, screenH * 0.2);
+    imagesMs = List.of(5000, 10000);
+    imagesAppearances.put(marsactuLogo, imagesMs);
+
+    System.out.println(imagesAppearances.get(iutLogo));
+    System.out.println(imagesAppearances.get(marsactuLogo));
+    System.out.println(textsAppearances.get(creators));
+    System.out.println(textsAppearances.get(forDaily));
+  
     Pane p = new Pane();
-    p.getChildren().addAll(mainBg, img.displayImage(), creators.displayText());
+    p.getChildren().addAll(mainBg, iutLogo.displayImage(), creators.displayText(), forDaily.displayText(), marsactuLogo.displayImage());
     
     Scene scene = new Scene(p,screenW,screenH);
     primaryStage.setTitle("A Simple Scene!");
@@ -47,20 +58,6 @@ public class Main extends Application {
     primaryStage.show();
 
     Timeline tmln = new Timeline(new KeyFrame(Duration.millis(1), event -> {
-      /*
-      if (mainMsTime <= 2000) {
-        creators.displayText().setOpacity(opc);
-        opc += 0.0005;
-      }
-
-      if (5000 <= mainMsTime && mainMsTime <= 7000) {
-        creators.displayText().setOpacity(opc);
-        opc -= 0.0005;
-      }
-      
-      title.setOpacity(objectOpacity(0, 2000, 0, 1, 0));
-      title.setOpacity(objectOpacity(5000, 7000, 1, 0, 0));
-      */
 
       ++mainMsTime;
     }));
