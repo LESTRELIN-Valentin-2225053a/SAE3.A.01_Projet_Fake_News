e8a2412df60b3deba3336ceae4d11e84b2365fd0
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-12-19 15:50:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,9 +25,36 @@ document.addEventListener("DOMContentLoaded", function() {
   const note = document.getElementById("note");
   const leftArrow = document.getElementById("leftArrow");
   const rightArrow = document.getElementById("rightArrow");
+  const playButton = document.getElementById("playButton");
+  const resumeButton = document.getElementById("resumeButton");
+  const doneButton = document.getElementById("doneButton");
+  const currentNoteText = document.getElementById("currentNoteText");
+
+  const mcThumbnail = document.getElementById("mcThumbnail");
+  const tTThumbnail = document.getElementById("tTThumbnail");
+  const fWThumbnail = document.getElementById("fWThumbnail");
+  const ivThumbnail = document.getElementById("ivThumbnail");
+  const cTThumbnail = document.getElementById("cTThumbnail");
+  const sOThumbnail = document.getElementById("sOThumbnail");
+  const c19Thumbnail = document.getElementById("c19Thumbnail");
+
+  const macron0 = document.getElementById("macron0");
+  const macron1 = document.getElementById("macron1");
+  const macron2 = document.getElementById("macron2");
+  const macron3 = document.getElementById("macron3");
   
   var t = 20000;
-
+  const cBCoords = [25, 10, 50, 50];
+  var notesList = {"Macron ou non ?" : [mcThumbnail, "todo"], //todo, acting, done
+                   "Trump et le réchauffement climatique" : [tTThumbnail, "todo"],
+                   "Les sites frauduleux" : [fWThumbnail, "todo"],
+                   "Vérification des faits d'un entretien" : [ivThumbnail, "todo"],
+                   "Texte à trous" : [cTThumbnail, "todo"],
+                   "Série de photos et vidéos" : [sOThumbnail, "todo"],
+                   "Covid-19" : [c19Thumbnail, "todo"]
+                   };
+  var noteIndex = 0;
+  
   function dimensions() {
     mainContainer.style.height = window.innerHeight + "px";
   }
@@ -69,6 +96,35 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
+  function displayNoteButton(notesListKey) {
+    imageParams(playButton, 100, 100, 5.21, 9.26);
+    imageParams(resumeButton, 100, 100, 5.21, 9.26);
+    imageParams(doneButton, 100, 100, 5.21, 9.26);
+    imgsOpct([playButton, resumeButton, doneButton], "0");
+    
+    if (notesList[notesListKey][1] == "todo") {
+      imageParams(playButton, 47.4, 85, 5.21, 9.26);
+      imgsOpct([playButton], "1");
+      detectElement(playButton);
+    }
+    else if (notesList[notesListKey][1] == "acting") {
+      imageParams(resumeButton, 47.4, 85, 5.21, 9.26);
+      imgsOpct([resumeButton], "1");
+      detectElement(resumeButton);
+    }
+    else if (notesList[notesListKey][1] == "done") {
+      imageParams(doneButton, 47.4, 85, 5.21, 9.26);
+      imgsOpct([doneButton], "1");
+      detectElement(doneButton);
+    }
+  }
+
+  function displayCurrentNoteText(noteTitle) {
+    currentNoteText.innerText = noteIndex + 1 + "/" + Object.keys(notesList).length + " " + noteTitle;
+    textParams(currentNoteText, 150, 0, 0, "Helvetica", 37.5, 10, 25, 8, 1.5);
+    imgsOpct([currentNoteText], "1");
+  }
+
   function detectElement(elmnt) {
     elmnt.addEventListener("mouseover", function() {
       imgsOpct([elmnt], "0.75");
@@ -79,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   
   function clickOnElement(elmnt) {
-    if (elmnt == computer || elmnt == shelf) {
+    if (elmnt == computer || elmnt == shelf || elmnt == corkBoard) {
       elmnt.addEventListener("click", function() {
         rectParams(obscurity, 0, 0);
         imgsOpct([obscurity], "0.75");
@@ -95,19 +151,75 @@ document.addEventListener("DOMContentLoaded", function() {
           imageParams(leftArrow, 25, 44.9, 3.65, 10.19);
           imageParams(rightArrow, 71.35, 44.9, 3.65, 10.19);
           imgsOpct([note, leftArrow, rightArrow], "1");
+          displayNoteButton(Object.keys(notesList)[noteIndex]);
+          displayCurrentNoteText(Object.keys(notesList)[noteIndex]);
+        
         }
+      });
+    }
+    if (elmnt == leftArrow) {
+      elmnt.addEventListener("click", function() {
+        --noteIndex;
+        if (noteIndex == -1) {
+          noteIndex = Object.keys(notesList).length - 1;
+        }
+        displayNoteButton(Object.keys(notesList)[noteIndex]);
+        displayCurrentNoteText(Object.keys(notesList)[noteIndex]);
+      });
+    }
+    if (elmnt == rightArrow) {
+      elmnt.addEventListener("click", function() {
+        ++noteIndex;
+        if (noteIndex == Object.keys(notesList).length) {
+          noteIndex = 0;
+        }
+        displayNoteButton(Object.keys(notesList)[noteIndex]);
+        displayCurrentNoteText(Object.keys(notesList)[noteIndex]);
+      });
+    }
+    if (elmnt == playButton) {
+      elmnt.addEventListener("click", function() {
+        for (let i = 0; i < Object.keys(notesList).length; ++i) {
+          if (notesList[Object.keys(notesList)[i]][1] == "acting") {
+            notesList[Object.keys(notesList)[i]][1] = "todo";
+          }
+        }
+        notesList[Object.keys(notesList)[noteIndex]][1] = "acting";
+        displayNoteButton(Object.keys(notesList)[noteIndex]);
+      });
+    }
+    if (elmnt == resumeButton) {
+      elmnt.addEventListener("click", function() {
+        notesList[Object.keys(notesList)[noteIndex]][1] = "todo";
+        displayNoteButton(Object.keys(notesList)[noteIndex]);
       });
     }
     if (elmnt == closeCross) {
       elmnt.addEventListener("click", function() {
-        imgsOpct([closeCross, computerScreen, note, leftArrow, rightArrow, obscurity], "0");
+        imgsOpct([closeCross, computerScreen, note, leftArrow, rightArrow, playButton, resumeButton, doneButton, obscurity], "0");
         imageParams(closeCross, 100, 100, 3.65, 6.48);
         imageParams(computerScreen, 100, 100, 60, 60);
         imageParams(note, 100, 100, 25, 62.96);
         imageParams(leftArrow, 100, 100, 3.65, 10.19);
         imageParams(rightArrow, 100, 100, 3.65, 10.19);
+        imageParams(playButton, 100, 100, 5.21, 9.26);
+        imageParams(resumeButton, 100, 100, 5.21, 9.26);
+        imageParams(doneButton, 100, 100, 5.21, 9.26);
+        textParams(currentNoteText, 150, 0, 0, "Helvetica", 100, 100, 25, 8, 1.5);
         rectParams(obscurity, 100, 100);
       });
+    }
+  }
+
+  const cBImages = {"Macron" : [[macron0, 20, 10, 20, 28.44],
+                                [macron1, 60, 20, 20, 13.33],
+                                [macron2, 30, 50, 20, 36.3],
+                                [macron3, 50, 60, 20, 20]]
+                   };
+
+  function displayCBImages(key) {
+    for (let i = 0; i < cBImages[key].length; ++i) {
+      imageParamscBImages[key][i];
     }
   }
   
@@ -128,14 +240,19 @@ document.addEventListener("DOMContentLoaded", function() {
   
   imageParams(whiteWall, 0, 0, 100, 100);
   imageParams(desk, 0, 75, 100, 25);
-  imageParams(corkBoard, 25, 10, 50, 50);
   
   detectElement(computer);
   detectElement(shelf);
+  detectElement(corkBoard);
   detectElement(leftArrow);
   detectElement(rightArrow);
   clickOnElement(computer);
   clickOnElement(shelf);
+  clickOnElement(corkBoard);
+  clickOnElement(leftArrow);
+  clickOnElement(rightArrow);
+  clickOnElement(playButton);
+  clickOnElement(resumeButton);
   clickOnElement(closeCross);
 
   
@@ -147,6 +264,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let imgsX = [movingImg0[0], movingImg1[0]];
     let intervalId;
     imgsOpct([titleBottomRect, titleExamples0, titleExamples1, titleUpRect, fekniouzeLogo, startText, imgsSubtitle], "0");
+
+    imageParams(corkBoard, cBCoords[0], cBCoords[1], cBCoords[2], cBCoords[3]);
     imageParams(computer, 25, 50, 40, 40);
     imageParams(shelf, 84, -15, 16, 100);
     imgsOpct([whiteWall, desk, corkBoard, computer, shelf], "1");
