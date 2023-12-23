52de2f7dfcf8e0ec8da247457961554f625c46e9
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-12-23 18:29:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,27 @@ document.addEventListener("DOMContentLoaded", function() {
   clickOnElement(resumeButton);
   clickOnElement(closeCross);
 
-  
+  const mc0Md = new Media(mc0, false, false, null, 20, 10, 20, 23.7);
+  const mc1Md = new Media(mc1, false, true, null, 40, 15, 20, 50.55);
+  const mc2Md = new Media(mc2, false, false, null, 65, 10, 20, 64.54);
+  const mc3Md = new Media(mc3, false, false, null, 10, 60, 20, 35.56);
+  const mc4Md = new Media(mc4, false, true, null, 75, 75, 20, 23.43);
+  const mc5Md = new Media(mc5, false, false, null, 35, 70, 20, 20);
+  const mc6Md = new Media(mc6, false, true, null, 5, 30, 20, 20.19);
+  const mcBoard = new BoardWithMedias([mc0Md, mc1Md, mc2Md, mc3Md, mc4Md, mc5Md, mc6Md]);
+
+  corkBoard.addEventListener("click", function() {
+    if (notesList[Object.keys(notesList)[0]][1] == "acting") {
+      mcBoard.showAll();
+    }
+  });
+
+  closeCross.addEventListener("click", function() {
+    const mcList = [mc0, mc1, mc2, mc3, mc4, mc5, mc6];
+    for (let i = 0; i < 7; ++i) {
+      imageParams(mcList[i], 0, 0, 0, 0);
+    }
+  });
   
   function displayRoom() {
     let movesNb = 0;
