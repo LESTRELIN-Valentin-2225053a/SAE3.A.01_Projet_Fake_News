7c97cf37a9220836f1278584112e14e833aef382
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-12-28 23:14:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -24,40 +24,61 @@ document.addEventListener("DOMContentLoaded", function() {
   
   imageParams(whiteWall, 0, 0, 100, 100);
   imageParams(desk, 0, 75, 100, 25);
-  
-  detectElement(computer);
-  detectElement(shelf);
-  detectElement(corkBoard);
-  detectElement(leftArrow);
-  detectElement(rightArrow);
-  clickOnElement(computer);
-  clickOnElement(shelf);
-  clickOnElement(corkBoard);
-  clickOnElement(leftArrow);
-  clickOnElement(rightArrow);
-  clickOnElement(playButton);
-  clickOnElement(resumeButton);
-  clickOnElement(closeCross);
 
-  const mc0Md = new Media(mc0, false, false, null, 20, 10, 20, 23.7);
-  const mc1Md = new Media(mc1, false, true, null, 40, 15, 20, 50.55);
-  const mc2Md = new Media(mc2, false, false, null, 65, 10, 20, 64.54);
-  const mc3Md = new Media(mc3, false, false, null, 10, 60, 20, 35.56);
-  const mc4Md = new Media(mc4, false, true, null, 75, 75, 20, 23.43);
-  const mc5Md = new Media(mc5, false, false, null, 35, 70, 20, 20);
-  const mc6Md = new Media(mc6, false, true, null, 5, 30, 20, 20.19);
+  const mc0Md = new Media(mc0, false, false, null, 20, 10, 20, 23.7, txtDict["mc0Md"]);
+  const mc1Md = new Media(mc1, false, true, null, 40, 15, 20, 50.55, txtDict["mc1Md"]);
+  const mc2Md = new Media(mc2, false, false, null, 65, 10, 20, 64.54, txtDict["mc2Md"]);
+  const mc3Md = new Media(mc3, false, false, null, 10, 60, 20, 35.56, txtDict["mc3Md"]);
+  const mc4Md = new Media(mc4, false, true, null, 75, 75, 20, 23.43, txtDict["mc4Md"]);
+  const mc5Md = new Media(mc5, false, false, null, 35, 70, 20, 20, txtDict["mc5Md"]);
+  const mc6Md = new Media(mc6, false, true, null, 5, 30, 20, 20.19, txtDict["mc6Md"]);
   const mcBoard = new BoardWithMedias([mc0Md, mc1Md, mc2Md, mc3Md, mc4Md, mc5Md, mc6Md]);
+
+  const gWT0Md = new Media(gWT0, false, true, null, 0, 20, 25, 60, "");
+  const gWT1Md = new Media(gWT1, false, false, null, 25, 20, 25, 60, "");
+  const gWT2Md = new Media(gWT2, false, false, null, 50, 20, 25, 60, "");
+  const gWT3Md = new Media(gWT3, false, false, null, 75, 20, 25, 60, "");
+  const gWTBoard = new BoardWithMedias([gWT0Md, gWT1Md, gWT2Md, gWT3Md]);
+
+  const tT0Md = new Media(tT0, false, null, null, 5, 10, 30, 20, "");
+  const tT1Md = new Media(tT1, false, false, null, 5, 30, 30, 20, "");
+  const tT2Md = new Media(tT2, false, false, null, 5, 50, 30, 20, "");
+  const tT3Md = new Media(tT3, false, true, null, 5, 70, 30, 20, "");
+  const tT4Md = new Media(tT4, false, true, null, 40, 10, 30, 20, "");
+  const tT5Md = new Media(tT5, false, null, null, 40, 30, 30, 20, "");
+  const tT6Md = new Media(tT6, false, null, null, 40, 50, 30, 20, "");
+  const tTBoard = new BoardWithMedias([tT0Md, tT1Md, tT2Md, tT3Md, tT4Md, tT5Md, tT6Md]);
+
+  dEList = [computer, shelf, corkBoard, leftArrow, rightArrow, mc0, mc1, mc2, mc3, mc4, mc5, mc6, gWT0, gWT1, gWT2, gWT3, tT0, tT1, tT2, tT3, tT4, tT5, tT6];
+  for (let i = 0; i < dEList.length; ++i) { detectElement(dEList[i]); }
+
+  cOEList = [computer, shelf, corkBoard, leftArrow, rightArrow, playButton, resumeButton, closeCross];
+  for (let i = 0; i < cOEList.length; ++i) { clickOnElement(cOEList[i]); }
 
   corkBoard.addEventListener("click", function() {
     if (notesList[Object.keys(notesList)[0]][1] == "acting") {
       mcBoard.showAll();
     }
+    else if (notesList[Object.keys(notesList)[1]][1] == "acting") {
+      gWTBoard.showAll();
+    }
+    else if (notesList[Object.keys(notesList)[2]][1] == "acting") {
+      tTBoard.showAll();
+    }
   });
 
   closeCross.addEventListener("click", function() {
     const mcList = [mc0, mc1, mc2, mc3, mc4, mc5, mc6];
-    for (let i = 0; i < 7; ++i) {
+    const gWTList = [gWT0, gWT1, gWT2, gWT3];
+    const tTList = [tT0, tT1, tT2, tT3, tT4, tT5, tT6];
+    for (let i = 0; i < mcList.length; ++i) {
       imageParams(mcList[i], 0, 0, 0, 0);
+    }
+    for (let i = 0; i < gWTList.length; ++i) {
+      imageParams(gWTList[i], 0, 0, 0, 0);
+    }
+    for (let i = 0; i < tTList.length; ++i) {
+      imageParams(tTList[i], 0, 0, 0, 0);
     }
   });
   
