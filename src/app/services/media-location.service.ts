9b4f426c551255bebe00d55055494036b3c6da71
ayoug <ayoub.essalhia@gmail.com>
2024-01-08 18:39:17 +0100

    changed medialocation description

--- src/app/services/media-location.service.ts
+++ src/app/services/media-location.service.ts
@@ -11,25 +11,25 @@ export class MediaLocationService {
       x : 0.02,
       y : 0.55,
       media : {id : -1} as Media,
-      description : 'zizi'
+      description : 'Mai 2017 : Photo d\'Emmanuel Macron rencontrant pour la première fois un dirigeant étranger. '
     },
     {
       x : 0.25,
       y : 0.65,
       media : {id : -1} as Media,
-      description : 'zizi'
+      description : '2'
     },
     {
       x : 0.45,
       y : 0.74,
       media : {id : -1} as Media,
-      description : 'zizi'
+      description : '3'
     },
     {
       x : 0.75,
       y : 0.6,
       media : {id : -1} as Media,
-      description : 'zizi'
+      description : '4'
     }
   ];
 
