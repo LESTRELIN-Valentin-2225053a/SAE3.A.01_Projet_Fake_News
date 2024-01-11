0d41b3bb842b5fa4ea859bf28ccae1afc369f7fd
ayoug <ayoub.essalhia@gmail.com>
2024-01-11 16:02:29 +0100

    added office component

---
+++ src/app/office/office.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-office',
+  standalone: true,
+  imports: [],
+  templateUrl: './office.component.html',
+  styleUrl: './office.component.css'
+})
+export class OfficeComponent {
+}
