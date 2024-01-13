1ee5f52f54289c9531c04f536870a45486cee7ff
Michaël Ditlecadet <ditlecadet.michael@gmail.com>
2024-01-13 20:43:00 +0100

    ⚠️IMPORTANT⚠️: Le bonsai bouge lorsqu'on clique dessus

--- src/app/office/office.component.ts
+++ src/app/office/office.component.ts
@@ -10,11 +10,18 @@ import {Component} from '@angular/core';
         <button class="computer"><img src="/assets/computer.png"/></button>
         <button class="book"><img src="/assets/book.png"/><p><strong>Enquête<br>en cours</strong></p></button>
         <button class="board"><img src="/assets/board.png"/></button>
-        <div class="bonsai"><img src="/assets/bonsai.png"/></div>
+        <div class="bonsai" (click)="shakeBonsai()"><img src="/assets/bonsai.png" /></div>
       </div>
     </div>`,
   standalone: true,
   styleUrls: ['./office.component.css']
 })
 export class OfficeComponent {
+  shakeBonsai() {
+    const bonsai = document.querySelector('.bonsai');
+    bonsai?.classList.add('clicked');
+    setTimeout(() => {
+      bonsai?.classList.remove('clicked');
+    }, 500);
+  }
 }
