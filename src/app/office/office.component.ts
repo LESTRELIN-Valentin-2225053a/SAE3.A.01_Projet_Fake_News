7508db3cfd9e5ae8663847fb85ae11d71ca762f0
Michaël Ditlecadet <ditlecadet.michael@gmail.com>
2024-01-14 20:06:15 +0100

    Correction avec le zoom de l'ordinateur

--- src/app/office/office.component.ts
+++ src/app/office/office.component.ts
@@ -6,24 +6,25 @@ import { Component } from '@angular/core';
     <div class="office">
       <div class="desk" [class.zoomed]="isComputerZoomed">
         <button class="computer" (click)="zoomComputer()">
-          <img src="/assets/computer.png"/>
+          <img src="/assets/computer.png" alt="Ordinateur"/>
         </button>
-        <button class="book"><img src="/assets/book.png"/>
+        <button class="book"><img src="/assets/book.png" alt="Livre d'enquêtes"/>
           <p><strong>Enquête<br>en cours</strong></p></button>
-        <button class="board"><img src="/assets/board.png"/></button>
-        <div class="bonsai" (click)="shakeBonsai()"><img src="/assets/bonsai.png"/></div>
+        <button class="board"><img src="/assets/board.png" alt="Tableau"/></button>
+        <div class="bonsai" (click)="shakeBonsai()"><img src="/assets/bonsai.png" alt="Arbre qui secoue lorsque l'on clique avec la souris et qui est pas mal"/></div>
       </div>
     </div>`,
   standalone: true,
+  imports: [
+  ],
   styleUrls: ['./office.component.css']
 })
 export class OfficeComponent {
-  isComputerZoomed = false;
 
+  isComputerZoomed = false;
   zoomComputer() {
     this.isComputerZoomed = true;
   }
-
   shakeBonsai() {
     const bonsai = document.querySelector('.bonsai');
     bonsai?.classList.add('clicked');
