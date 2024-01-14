00075523bfee80514ad03a34a3a7e40879f22d2f
Michaël Ditlecadet <ditlecadet.michael@gmail.com>
2024-01-14 14:43:43 +0100

    Zoom sur l'ordinateur lorsque l'on clique dessus (il faut juste que je rajoute une flèche pour retourner en arrière)

--- src/app/office/office.component.ts
+++ src/app/office/office.component.ts
@@ -1,22 +1,29 @@
-// office.component.ts
-
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-office',
   template: `
     <div class="office">
-      <div class="desk">
-        <button class="computer"><img src="/assets/computer.png"/></button>
-        <button class="book"><img src="/assets/book.png"/><p><strong>Enquête<br>en cours</strong></p></button>
+      <div class="desk" [class.zoomed]="isComputerZoomed">
+        <button class="computer" (click)="zoomComputer()">
+          <img src="/assets/computer.png"/>
+        </button>
+        <button class="book"><img src="/assets/book.png"/>
+          <p><strong>Enquête<br>en cours</strong></p></button>
         <button class="board"><img src="/assets/board.png"/></button>
-        <div class="bonsai" (click)="shakeBonsai()"><img src="/assets/bonsai.png" /></div>
+        <div class="bonsai" (click)="shakeBonsai()"><img src="/assets/bonsai.png"/></div>
       </div>
     </div>`,
   standalone: true,
   styleUrls: ['./office.component.css']
 })
 export class OfficeComponent {
+  isComputerZoomed = false;
+
+  zoomComputer() {
+    this.isComputerZoomed = true;
+  }
+
   shakeBonsai() {
     const bonsai = document.querySelector('.bonsai');
     bonsai?.classList.add('clicked');
