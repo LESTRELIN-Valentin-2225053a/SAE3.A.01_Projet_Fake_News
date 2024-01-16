b6752f7c1d1f8ba633417eee3fb76fd433274729
ayoug <ayoub.essalhia@gmail.com>
2024-01-16 14:38:35 +0100

    added route to computer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {BoardWithDraggableMediasComponent} from "./board-with-draggable-medias/board-with-draggable-medias.component";
 import {OfficeComponent} from "./office/office.component";
+import {ComputerComponent} from "./computer/computer.component";
 
 export const routes: Routes = [
   {path: '',component: OfficeComponent},
-  {path: 'board',component: BoardWithDraggableMediasComponent}
+  {path: 'board',component: BoardWithDraggableMediasComponent},
+  {path: 'computer',component: ComputerComponent}
 ];
