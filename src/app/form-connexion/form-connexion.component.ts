a423e3b5770c536800fa356e1ea71d4a8b1cac2d
Rackyrr <116524047+Rackyrr@users.noreply.github.com>
2024-01-17 04:39:49 +0100

    Add connexion to service (form register/login)

--- src/app/form-connexion/form-connexion.component.ts
+++ src/app/form-connexion/form-connexion.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import {NgIf} from "@angular/common";
+import {CommonModule, NgIf} from "@angular/common";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {APIService} from "../services/apiservice.service";
 
 @Component({
   selector: 'app-form-connexion',
@@ -9,16 +11,48 @@ import {NgIf} from "@angular/common";
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    NgIf
+    NgIf,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './form-connexion.component.html',
   styleUrl: './form-connexion.component.css'
 })
 export class FormConnexionComponent {
-  showInsciption: boolean = false;
+  registerForm = new FormGroup({
+    registerName: new FormControl(''),
+    registerEmail: new FormControl(''),
+    registerPassword: new FormControl(''),
+    registerPasswordVerif: new FormControl(''),
+  });
 
+  loginForm= new FormGroup({
+    loginEmail: new FormControl(''),
+    loginPassword: new FormControl('')
+  });
+
+  showInsciption: boolean = true;
+
+  apiService : APIService = inject(APIService);
 
   showInscriptionToggled(): void {
     this.showInsciption = !this.showInsciption;
+  }
+
+  sendLoginForm(){
+
+    this.apiService.login(this.loginForm).then(response => {
+      console.log('Connexion réussie', response);
+    }).catch(error => {
+      console.error('Erreur lors de la connexion', error);
+    });
+  }
+
+  sendRegisterForm(){
+    this.apiService.register(this.registerForm).then(response => {
+      console.log('Inscription réussie', response);
+    }).catch(error => {
+      console.error('Erreur lors de l\'inscription', error);
+    });
   }
 }
