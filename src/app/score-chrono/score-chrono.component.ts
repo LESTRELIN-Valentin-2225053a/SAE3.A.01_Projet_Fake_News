9cb2e0e2c492b3280b867c4eb48c290e09c06104
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2024-02-26 15:59:39 +0100

    Update score-chrono.component.ts

--- src/app/score-chrono/score-chrono.component.ts
+++ src/app/score-chrono/score-chrono.component.ts
@@ -6,45 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./score-chrono.component.css']
 })
 export class ScoreChronoComponent {
-  gameScore: number;
-  timer: any;
-  elapsedTime: number;
-
-  constructor() {
-    this.gameScore = 0;
-    this.timer = 0;
-    this.elapsedTime = 0;
-  }
-
-  initScore() {
-    this.gameScore = 0;
-  }
-
-  increaseScore(plus: number) {
-    this.gameScore += plus;
-  }
-
-  getScore() {
-    return this.gameScore;
-  }
-
-  startTimer() {
-    this.timer = setInterval(() => {
-      this.elapsedTime++;
-    }, 1000);
-  }
-
-  stopTimer() {
-    clearInterval(this.timer);
-  }
-
-  resetTimer() {
-    this.elapsedTime = 0;
-  }
-
-  getElapsedTime() {
-    return Math.floor(this.elapsedTime / 3600) + "h " + 
-           Math.floor((this.elapsedTime % 3600) / 60) + "m " +
-           this.elapsedTime % 60 + "s";
-  }
 }
