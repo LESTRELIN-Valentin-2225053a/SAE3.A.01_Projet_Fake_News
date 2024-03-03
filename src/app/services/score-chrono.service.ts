e9433348279b73e196c853c1c61d9a07dd65aed6
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2024-03-03 21:13:00 +0100

    Update score-chrono.service.ts

--- src/app/services/score-chrono.service.ts
+++ src/app/services/score-chrono.service.ts
@@ -4,35 +4,50 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ScoreChronoService {
+  completedInvestigations = 0;
   gameScore = 0;
   elapsedTime = 0;
   timer: any;
+  listElements: { elapsedTime: string, score: number }[] = [];
+  scoreHasChanged: boolean = false;
 
-  setScore(score: number) {
-    this.gameScore = score;
+  reset(): void {
+    clearInterval(this.timer);
+    this.completedInvestigations = 0;
+    this.gameScore = 0;
+    this.elapsedTime = 0;
   }
 
-  getScore() {
-    return this.gameScore;
-  }
-
-  startTimer() {
+  startTimer(): void {
     this.timer = setInterval(() => {
       ++this.elapsedTime;
     }, 1000);
   }
 
-  stopTimer() {
-    clearInterval(this.timer);
+  oneMoreInvestigation(): void {
+    ++this.completedInvestigations;
   }
 
-  resetTimer() {
-    this.elapsedTime = 0;
+  addScore(score: number) {
+    this.gameScore = score;
+    this.scoreHasChanged = true;
   }
 
-  getElapsedTime() {
+  getCompletedInvestigationsNumber(): number {
+    return this.completedInvestigations;
+  }
+
+  getScore(): number {
+    return this.gameScore;
+  }
+
+  getElapsedTime(): string {
     return Math.floor(this.elapsedTime / 3600) + "h " + 
            Math.floor((this.elapsedTime % 3600) / 60) + "m " +
            this.elapsedTime % 60 + "s";
+  }
+
+  getListElements() {
+    return this.listElements;
   }
 }
