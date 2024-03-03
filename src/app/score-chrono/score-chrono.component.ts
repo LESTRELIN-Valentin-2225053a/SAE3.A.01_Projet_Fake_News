6f65351e8e68b8ef58b4c4acd534fe40eb66da6f
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2024-03-03 21:10:22 +0100

    Update score-chrono.component.ts

--- src/app/score-chrono/score-chrono.component.ts
+++ src/app/score-chrono/score-chrono.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ScoreChronoService } from '../services/score-chrono.service';
+import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-score',
+  selector: 'app-score-chrono',
   templateUrl: './score-chrono.component.html',
+  imports: [
+    NgFor,
+  ],
+  standalone: true,
   styleUrls: ['./score-chrono.component.css']
 })
 export class ScoreChronoComponent {
+
+  scoreChronoService: ScoreChronoService = inject(ScoreChronoService);
+  listElements: { elapsedTime: string, score: number }[] = [];
+
+  ngOnInit(): void {
+    if (this.scoreChronoService.scoreHasChanged == true) {
+      this.addNewListElement();
+      this.scoreChronoService.scoreHasChanged = false;
+    }
+  }
+
+  addNewListElement(): void {
+    const elapsedTime = this.scoreChronoService.getElapsedTime();
+    const score = this.scoreChronoService.getScore();
+    this.scoreChronoService.listElements.unshift({ elapsedTime, score });
+  }
+
+  completedInvestigations(): number {
+    return this.scoreChronoService.getCompletedInvestigationsNumber();
+  }
+
+  getListElements(): any {
+    return this.scoreChronoService.getListElements();
+  }
 }
